8e3f7f2c5b42c2972459960a0eeffa9b5ac6001c
RuJi <rujic4@hotmail.com>
2016-12-16 03:06:03 +0000

    P1 Push 2

--- hello-cpp-world.cc
+++ hello-cpp-world.cc
@@ -7,17 +7,36 @@ using namespace std;
 
 class Lotto{
         public :        
-            int lotto;
+            int lotto,lotto2,lotto3;
             
         public:    
             int run_1st(){
-                srand(time(NULL));
                 lotto = rand()%1000000;
+                for(;lotto<100000;)
+                    lotto = rand()%1000000;
+                
                 return lotto;
+            }
+            int run_2nd(){
+                lotto2 = rand()%100000;
+                for(;lotto2<10000||lotto2>=100000;)
+                    lotto2 = rand()%100000;
+                
+                return lotto2;
+            }
+            int run_3rd(){
+                lotto3 = rand()%10000;
+                for(;lotto3<1000||lotto3>=10000;)
+                    lotto3 = rand()%10000;
+                
+                return lotto3;
             }
     };
     
 int main() {
     Lotto lot;
-    cout << lot.run_1st();
+    srand(time(NULL));
+    cout << lot.run_1st()<<endl;
+    cout << lot.run_2nd()<<endl;
+    cout << lot.run_3rd()<<endl;
 }
