5adcae010de5e9725af3a451892039d57c28b13c
Ratchapon <yo.ratchapon_st@tni.ac.th>
2016-12-16 02:49:54 +0000

    P2 Push 1

--- hello-cpp-world.cc
+++ hello-cpp-world.cc
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
+using namespace std;
+
+
+class Lotto{
+        public :        
+            int lotto;
+            
+        public:    
+            int run_1st(){
+                srand(time(NULL));
+                lotto = rand()%1000000;
+                return lotto;
+            }
+    };
+    
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    Lotto lot;
+    cout << lot.run_1st();
 }
